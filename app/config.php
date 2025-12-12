b0017109d2ed88eda891d68ed5b2f12dc11b4310
Wally Jay Tech <wallyjay111@gmail.com>
2025-12-13 00:34:04 +0100

    ✌𝓦𝓐𝓛𝓛𝓨𝓙𝓐𝓨𝓣𝓔𝓒𝓗-𝓜𝓓✌

--- app/config.php
+++ app/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_HOST', '127.0.0.1');
-define('DB_USER', 'root');
-define('DB_PASS', 'yourpass');
-define('DB_NAME', 'smm_free');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'paneluser');
+define('DB_PASS', 'Wallyartmedia1@');
+define('DB_NAME', 'smmpanel');
 define('TIMEZONE', 'Asia/Ho_Chi_Minh');
 define('ENCRYPTION_KEY', '202dd507882ef55dbbc23f7e84dcfc8d');
